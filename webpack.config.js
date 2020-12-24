3ed5d2e41f7243a99d0435420cfbfd82a9837b7f
Mostafa <mos.sameti@gmail.com>
2020-12-25 01:21:31 +0330

    add webpack & babel configs

---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const path = require('path');
+const dotenv = require('dotenv');
+const webpack = require('webpack');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+const ErrorOverlayPlugin = require('error-overlay-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+const env = dotenv.config().parsed;
+
+const envKeys = Object.keys(env).reduce((prev, next) => {
+  prev[`process.env.${next}`] = JSON.stringify(env[next]);
+  return prev;
+}, {});
+
+module.exports = {
+  entry: path.resolve(__dirname, './src/index.tsx'),
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename:
+      env.NODE_ENV === 'production'
+        ? '[name].[contenthash].bundle.js'
+        : '[name].[hash].bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json'],
+    plugins: [new TsconfigPathsPlugin({ configFile: 'tsconfig.json' })],
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'dist'),
+    port: 3000,
+    historyApiFallback: true,
+    host: 'localhost',
+  },
+  optimization: {
+    moduleIds: 'hashed',
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
+  },
+  plugins: [
+    new webpack.DefinePlugin(envKeys),
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: 'public/index.html', //source html
+      favicon: 'src/static/images/favicon.png'
+    }),
+    new ErrorOverlayPlugin(),
+  ],
+};
