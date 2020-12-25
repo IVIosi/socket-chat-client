00f02b5e8a5142feaa825e97d6552e189dce92fb
Mostafa <mos.sameti@gmail.com>
2020-12-26 01:31:01 +0330

    add modal component

---
+++ src/components/ui-kit/modal/index.tsx
@@ -0,0 +1,70 @@
+import React, { FC, ReactNode, useEffect, useState } from 'react';
+import { createPortal } from 'react-dom';
+import './style.scss';
+
+const Portal: FC = ({ children }) => {
+  let modalRoot = document.getElementById('modal');
+
+  if (!modalRoot) {
+    modalRoot = document.createElement('div');
+    modalRoot.setAttribute('id', 'modal');
+    document.body.appendChild(modalRoot);
+  }
+
+  const modalElement = document.createElement('div');
+
+  useEffect(() => {
+    modalRoot?.appendChild(modalElement);
+    return () => {
+      modalRoot?.removeChild(modalElement);
+    };
+  });
+
+  return createPortal(children, modalElement);
+};
+
+interface ModalHook {
+  open: boolean;
+  close: boolean;
+  openModal: () => void;
+  closeModal: () => void;
+}
+
+export const useModal = (): ModalHook => {
+  const [open, onOpenModal] = useState(false);
+  const [close, onCloseModal] = useState(false);
+
+  const openModal = () => {
+    onOpenModal(true);
+  };
+
+  const closeModal = () => {
+    onCloseModal(true);
+    onOpenModal(false);
+  };
+
+  return { open, close, openModal, closeModal };
+};
+
+interface ModalProps {
+  close: () => void;
+  render: (c: ReactNode) => ReactNode;
+}
+
+const Modal: FC<ModalProps> = ({ children, close, render }) => {
+  return (
+    <Portal>
+      <div className="modal">
+        <div className="modal__parent">
+          <div className="modal__header">
+            <button className="modal__close-button" onClick={close} />
+          </div>
+          {render(children) || children}
+        </div>
+      </div>
+      <div className="modal__overlay" onClick={close} />
+    </Portal>
+  );
+};
+
+export default Modal;
