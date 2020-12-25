84f7b1e139542cf47019e5eb5659a27c99a0f787
Mostafa <mos.sameti@gmail.com>
2020-12-25 21:07:05 +0330

    app main component

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,20 @@
 import React, { FC } from 'react';
 import './style.scss';
 
+import Header from '@components/header';
+import Messages from '@components/messages';
+import SendMessageBox from '@components/send-message-box';
+
 const App: FC = () => {
-  return <h1>socket chat app</h1>;
+  return (
+    <>
+      <Header />
+      <main className="main">
+        <Messages />
+        <SendMessageBox />
+      </main>
+    </>
+  );
 };
 
 export default App;
