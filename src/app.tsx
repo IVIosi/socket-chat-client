482eda69bf7991e70441ea973625f2d2d9f0adbc
Mostafa <mos.sameti@gmail.com>
2020-12-26 03:51:07 +0330

    add settings context

--- src/app.tsx
+++ src/app.tsx
@@ -1,19 +1,41 @@
-import React, { FC } from 'react';
+import React, { createContext, FC } from 'react';
 import './style.scss';
 
+import { useLocalStorage } from '@helpers/hooks-helper';
 import Header from '@components/header';
 import Messages from '@components/messages';
 import SendMessageBox from '@components/send-message-box';
 
+export interface Settings {
+  clock: '12H' | '24H';
+  sendWith: 'enter' | 'cmd+enter';
+  userName: string;
+}
+
+export const defaultSettings: Settings = {
+  clock: '12H',
+  sendWith: 'enter',
+  userName: 'New User',
+};
+
+export const SettingsContext = createContext({
+  settings: defaultSettings,
+  setSettings: (_s: Settings) => {
+    return;
+  },
+});
+
 const App: FC = () => {
+  const [settings, setSettings] = useLocalStorage<Settings>('settings', defaultSettings);
+
   return (
-    <>
+    <SettingsContext.Provider value={{ settings, setSettings }}>
       <Header />
       <main className="main">
         <Messages />
         <SendMessageBox />
       </main>
-    </>
+    </SettingsContext.Provider>
   );
 };
 
--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,28 +1,25 @@
-import React, { FC } from 'react';
+import React, { FC, useContext } from 'react';
 import './style.scss';
 
 import Button from '@components/ui-kit/button';
 import Modal, { useModal } from '@components/ui-kit/modal';
 import RadioButton from '@components/ui-kit/radio-button';
-import { useLocalStorage } from '@helpers/hooks-helper';
-
-interface Settings {
-  clock: '12H' | '24H';
-  sendWith: 'enter' | 'cmd+enter';
-}
+import Input from '@components/ui-kit/input';
+import { SettingsContext, Settings, defaultSettings } from 'app';
 
 const Header: FC = () => {
+  const { settings, setSettings } = useContext(SettingsContext);
   const { open, openModal, closeModal } = useModal();
-  const [settings, setSettings] = useLocalStorage<Settings>('settings', {
-    clock: '12H',
-    sendWith: 'enter',
-  });
 
-  const handleUpdateSettings = (key: string, value: string) => {
+  const handleUpdateSettings = (key: keyof Settings, value: string) => {
     setSettings({
       ...settings,
       [key]: value,
     });
+  };
+
+  const handleResetSettings = () => {
+    setSettings(defaultSettings);
   };
 
   return (
@@ -31,7 +28,6 @@ const Header: FC = () => {
       <div className="header__settings">
         <Button
           type="secondary"
-          text="Settings"
           onClick={openModal}
           icon={
             <svg
@@ -49,13 +45,20 @@ const Header: FC = () => {
               <path d="M19.4 15a1.65 1.65 0 0 0 .33 1.82l.06.06a2 2 0 0 1 0 2.83 2 2 0 0 1-2.83 0l-.06-.06a1.65 1.65 0 0 0-1.82-.33 1.65 1.65 0 0 0-1 1.51V21a2 2 0 0 1-2 2 2 2 0 0 1-2-2v-.09A1.65 1.65 0 0 0 9 19.4a1.65 1.65 0 0 0-1.82.33l-.06.06a2 2 0 0 1-2.83 0 2 2 0 0 1 0-2.83l.06-.06a1.65 1.65 0 0 0 .33-1.82 1.65 1.65 0 0 0-1.51-1H3a2 2 0 0 1-2-2 2 2 0 0 1 2-2h.09A1.65 1.65 0 0 0 4.6 9a1.65 1.65 0 0 0-.33-1.82l-.06-.06a2 2 0 0 1 0-2.83 2 2 0 0 1 2.83 0l.06.06a1.65 1.65 0 0 0 1.82.33H9a1.65 1.65 0 0 0 1-1.51V3a2 2 0 0 1 2-2 2 2 0 0 1 2 2v.09a1.65 1.65 0 0 0 1 1.51 1.65 1.65 0 0 0 1.82-.33l.06-.06a2 2 0 0 1 2.83 0 2 2 0 0 1 0 2.83l-.06.06a1.65 1.65 0 0 0-.33 1.82V9a1.65 1.65 0 0 0 1.51 1H21a2 2 0 0 1 2 2 2 2 0 0 1-2 2h-.09a1.65 1.65 0 0 0-1.51 1z" />
             </svg>
           }
-        />
+        >
+          Settings
+        </Button>
       </div>
       {open ? (
         <Modal
           close={closeModal}
           render={() => (
-            <div>
+            <>
+              <p>Username:</p>
+              <Input
+                defaultValue={settings.userName}
+                onChange={(v) => handleUpdateSettings('userName', v)}
+              />
               <p>Clock Display:</p>
               <RadioButton
                 name="Clock"
@@ -88,7 +91,10 @@ const Header: FC = () => {
                 ]}
                 onChange={(v) => handleUpdateSettings('sendWith', v)}
               />
-            </div>
+              <Button type="secondary" onClick={handleResetSettings}>
+                Reset to default
+              </Button>
+            </>
           )}
         />
       ) : null}
