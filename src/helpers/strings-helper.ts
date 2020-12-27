c46072ccc33604f61cc976a5a16f5ef2f81c4b83
Mostafa <mos.sameti@gmail.com>
2020-12-28 00:29:34 +0330

    fix minutes smaller than 10 not shown correctly

--- src/helpers/strings-helper.ts
+++ src/helpers/strings-helper.ts
@@ -2,15 +2,15 @@ export const formatTime = (unixTimestamp: number, twelveHour: boolean): string =
   const date = new Date(unixTimestamp);
   let hours = date.getHours();
   const minutes = date.getMinutes();
+  const min = minutes < 10 ? `0${minutes}` : minutes;
 
   if (twelveHour) {
     const ampm = hours >= 12 ? 'PM' : 'AM';
     hours = hours % 12;
     hours = hours ? hours : 12; // the hour '0' should be '12'
-    const min = minutes < 10 ? `0${minutes}` : minutes;
     const strTime = `${hours}:${min} ${ampm}`;
     return strTime;
   } else {
-    return `${hours}:${minutes}`;
+    return `${hours}:${min}`;
   }
 };
