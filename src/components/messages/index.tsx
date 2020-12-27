637817618286b854a048325baa73a0c5b6495b93
Mostafa Sameti <mos.sameti@gmail.com>
2020-12-27 12:11:44 +0330

    show user name on every meesage

--- src/components/messages/index.tsx
+++ src/components/messages/index.tsx
@@ -14,30 +14,33 @@ const MessagesList: FC<MessagesListProps> = ({ messages }) => {
 
   return (
     <div className="messages">
-      {messages.map((message) => (
-        <div
-          key={message.time}
-          className={`message ${
-            message.userID === settings.userID ? 'message--sent' : 'message--received'
-          }`}
-        >
-          <div className="message__avatar">
-            <img src={message?.avatar || defaultAvatar} />
-          </div>
-          <div className="message-content">
-            <div className="message-content__text">
-              {message.type === 'text' ? (
-                message.content
-              ) : (
-                <img className="message-content__image" src={message.content} />
-              )}
+      {messages.map((message) => {
+        const isSent = message.userID === settings.userID;
+
+        return (
+          <div
+            key={message.time}
+            className={`message ${isSent ? 'message--sent' : 'message--received'}`}
+          >
+            <div className="message__avatar">
+              <img src={message?.avatar || defaultAvatar} />
             </div>
-            <div className="message-content__time">
-              {formatTime(message.time, settings.clock === '12H')}
+            <div className="message-content">
+              <div className="message-content__text">
+                {!isSent && <div className="message-content__username">{message.userName}</div>}
+                {message.type === 'text' ? (
+                  message.content
+                ) : (
+                  <img className="message-content__image" src={message.content} />
+                )}
+              </div>
+              <div className="message-content__time">
+                {formatTime(message.time, settings.clock === '12H')}
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
