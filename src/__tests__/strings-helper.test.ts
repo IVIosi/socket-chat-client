08533c488918fc8403900fed290f4895482465bd
Mostafa Sameti <mos.sameti@gmail.com>
2020-12-28 13:08:31 +0330

    add two digits test for format time function

--- src/__tests__/strings-helper.test.ts
+++ src/__tests__/strings-helper.test.ts
@@ -9,3 +9,9 @@ test('It should return time in 24H format', () => {
   const time = new Date(1995, 11, 17, 15, 24, 0).getTime();
   expect(formatTime(time, false)).toBe('15:24');
 });
+
+test('It should return minutes below 10 with two digits', () => {
+  const time = new Date(1995, 11, 17, 3, 2, 0).getTime();
+  expect(formatTime(time, true)).toBe('3:02 AM');
+  expect(formatTime(time, false)).toBe('3:02');
+});
